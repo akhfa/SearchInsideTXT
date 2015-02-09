871831380c1a7630ccdaf450420c2089cc8983d3
akhfa <akhmadfakhoni@gmail.com>
2015-02-09 14:46:09 +0700

    init

---
+++ search.c
@@ -0,0 +1,24 @@
+// reading a text file
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main () {
+  string line;
+  ifstream myfile ("example.txt");
+  if (myfile.is_open())
+  {
+    while ( getline (myfile,line) )
+    {
+      system(line.c_str());
+    }
+    myfile.close();
+  }
+
+  else cout << "Unable to open file"; 
+
+  return 0;
+}
